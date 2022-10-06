09a09797a67592207e9d271e98587b6b03fde34a
Maxim Myasischev <mnmyasis@gmail.com>
2022-10-06 15:27:09 +0300

    feat(users-app): Create users app.
    - Добавлена страница логина.

---
+++ assistant_accountant/users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.contrib.auth.views import (LogoutView,
+                                       LoginView, )
+
+app_name = 'users'
+
+urlpatterns = [
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+]

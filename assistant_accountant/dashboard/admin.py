2c45eed9d61398da7d0961f4e0a9bbae1505cd83
Maxim Myasischev <mnmyasis@gmail.com>
2022-11-08 00:55:17 +0300

    feate(models): Create models.
    - Описаны модели
    - Новые модели добавлены в админку
    - Изменены тестовые вью.
    - Старые модели удалены

--- assistant_accountant/dashboard/admin.py
+++ assistant_accountant/dashboard/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.YandexDirectToken)
-admin.site.register(models.VkAdsToken)
-admin.site.register(models.MyTargetToken)
+admin.site.register(models.Source)
+admin.site.register(models.Token)
+admin.site.register(models.VkAccount)
+admin.site.register(models.Campaign)
+admin.site.register(models.AgencyClient)
+admin.site.register(models.BalanceHistory)
+admin.site.register(models.StatisticByAgencyClient)

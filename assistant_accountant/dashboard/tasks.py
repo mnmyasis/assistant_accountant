56d63132f088de0f84b1b4bcc955f8d525479505
Maxim Myasischev <mnmyasis@gmail.com>
2022-11-09 16:53:49 +0300

    feat(tasks): Collect stats vk, my-target
    - Добавлен сбор статистики VK, myTarget
    - Сырые данные приведены к общему типу

--- assistant_accountant/dashboard/tasks.py
+++ assistant_accountant/dashboard/tasks.py
@@ -2,20 +2,21 @@ import json
 
 from celery import shared_task
 
-from .ads import (API, YandexAgencyClients, VKAgencyClients,
-                  MyTargetAgencyClients)
+from .ads import (API, YandexAgencyClients, VKCollectData,
+                  MyTargetCollectData)
 
 
 @shared_task(name='agency_clients')
 def agency_clients(user_id: int):
+    date_from = '2022-11-01'
+    date_to = '2022-11-09'
     yandex = YandexAgencyClients(user_id, on_sandbox=True)
-    vk = VKAgencyClients(user_id)
-    my_target = MyTargetAgencyClients(user_id)
+    vk = VKCollectData(user_id, date_from, date_to)
+    my_target = MyTargetCollectData(user_id, date_from, date_to)
     cabinets = [yandex, my_target, vk]
     data = []
     for cabinet in cabinets:
         api = API(cabinet)
-        data += api.agency_clients()
-    for line in data:
-        print(json.dumps(line, indent=4))
+        data += api.collect_data_ads()
+    print(json.dumps(data, indent=4))
     return data
